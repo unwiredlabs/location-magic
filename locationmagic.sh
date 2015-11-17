ae0a1f06942a029691a04096d2fa27fbf2b5993a
Gopi Aravind <g@unwiredlabs.com>
2015-11-17 19:46:14 +0530

    Fixes for distros where installation would fail
    Distros without /usr/local/bin would cause installation to fail. Other minor fixes for Linux platforms,
     including full path to the iwlist

--- locationmagic.sh
+++ locationmagic.sh
@@ -1,10 +1,14 @@
-####################################
-# X-Powered-By: Unwired Labs	   #
-# Version 0.2			   		   #
-####################################
+#!/bin/bash
+
+##############################################################
+# Powered-By: Unwired Labs                                   #
+# Version 1.1                                                #
+# run as ./locationmagic.sh -locate [PLATFORM] [TOKEN]       #
+##############################################################
+
 set -e
 
-version=0.2
+version=1.1
 echo "Welcome to LocationMagic " + $version + "
 ...a simple way to track your *nix device.
 
@@ -19,28 +23,29 @@ log_file=/var/log/locationmagic/locationmagic.log
 
 HELP="Usage: <script> <function> <platform> <token>
  Functions:
- -install = install the script 
- -uninstall = unintsall the script 
- -locate = locate the device
+ -install   = install the script
+ -uninstall = unintsall the script
+ -locate    = locate the device
 
 Platform:
-osx   = for macbooks
-linux = for Linux (Ubuntu, etc)
+osx   = for MacBooks
+linux = for Linux (Ubuntu, Raspberry Pi, Arduino etc)
 
 Token = your token generated at https://locationmagic.org
 
 Examples:
-To install   = ./locationmagic_mac.sh -install osx xyz12345aabbcc
-To uninstall = ./locationmagic_mac.sh -uninstall
-To locate    = ./locationmagic_mac.sh -locate osx xyz12345aabbcc
+To install   = ./locationmagic.sh -install osx xyz12345aabbcc
+To uninstall = ./locationmagic.sh -uninstall
+To locate    = ./locationmagic.sh -locate osx xyz12345aabbcc
 "
 
 if [ "$inputargs" = "-install" ] || [ "$inputargs" = "-locate" ]
-then	
+then
 	if [ ! -f /usr/local/bin/locationmagic.sh ]; then
 		echo "Copying script to /usr/local/bin.."
+        	mkdir -p /usr/local/bin
 		cp locationmagic.sh /usr/local/bin/
-		chmod +x /usr/local/bin/locationmagic.sh	
+		chmod +x /usr/local/bin/locationmagic.sh
 	fi
 
 	if [ ! -f /var/log/locationmagic/locationmagic.log ]; then
@@ -59,17 +64,17 @@ then
 		echo "Backing up crontab.."
 		crontab -l > crontab_$(whoami).backup
 		echo "Adding script to run every hour"
-		($(crontab -l > file; echo "0 * * * * cd /usr/local/bin/ && ./locationmagic.sh -locate $platform $token" >> file | sort file | uniq file | crontab -)) 	
+		($(crontab -l > file; echo "0 * * * * cd /usr/local/bin/ && ./locationmagic.sh -locate $platform $token" >> file | sort file | uniq file | crontab -))
 		echo "All done!"
 		echo $stamp" Installed Successfully" >> $log_file
 	fi
 
 	echo "Searching for WiFi networks.."
 	if [ "$platform" = "osx" ]; then
-		list=($(/System/Library/PrivateFrameworks/Apple80211.framework/Versions/Current/Resources/airport -s |  egrep -o '([a-f0-9]{2}:){5}[a-f0-9]{2}'))	
+		list=($(/System/Library/PrivateFrameworks/Apple80211.framework/Versions/Current/Resources/airport -s |  egrep -o '([a-f0-9]{2}:){5}[a-f0-9]{2}'))
 	elif [ "$platform" = "linux" ]; then
-		interface=($(ifconfig | grep wlan | awk '{print $1}'))
-		list=($(iwlist "$interface" scanning | grep Address | awk '{print $5}'))
+		interface=($(/sbin/ifconfig | grep wlan | awk '{print $1}'))
+		list=($(/sbin/iwlist "$interface" scanning | grep Address | awk '{print $5}'))
 	else
 		echo "Invalid platform / OS selected.. Proceeding with IP address based location"
 		list[0]=""
@@ -95,21 +100,19 @@ then
 	input="${input%?}"
 
 	output=$(curl --silent $input)
-	
+
 	echo $output
 
 	echo $stamp $output >> $log_file
 
 	echo "Installation complete.."
-	
+
 elif [ "$inputargs" = "-uninstall" ]
 then
 	comment=($(crontab -l | sed "/^[^#].*locationmagic.sh*/s/^/#/" | crontab - ))
 	sudo rm /usr/local/bin/locationmagic.sh
 	echo "Uninstalled successfully"
 	echo $stamp" Uninstalled Successfully" >> $log_file
-	exit
 else
 	echo "$HELP"
-	exit
 fi
